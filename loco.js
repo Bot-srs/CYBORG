956ff8c6c95b28ddeff54fda8aacdf9e0e6a86b7
srs <satapathysoumyaranjan248@gmail.com>
2024-02-08 09:47:22 +0530

    srs

---
+++ loco.js
@@ -0,0 +1,8 @@
+// SMOOTH SCROOLING
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true,
+    lerp: 0.03//lerp is to control the smooth scrolling effect
+}); // this code is taken from - Locomotive js github-->scroll down-->under the SMOOTH section excluding the import part copied the code and paste here in java script
+a = prompt("ENTER YOUR ID ");
+console.log(alert("WELCOME CYBORG"+a));
